198773e0542ce2698e27ae0f366dd01c6f512064
Roberto Torella <roberto.torella@gmail.com>
2013-09-23 17:45:19 +0100

    CCPlus now more or less live

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,7 +2,7 @@ javascript: void((function(d){
 	if (!document.querySelector("#ccplus-style")) {
 		var s = d.createElement("script");
 		s.id="ccplus-style";
-		s.src = "http://ccplus.localhost/ccplus.js";
+		s.src = "https://raw.github.com/ganglio/ccplus/master/ccplus.js";
 		s.type="text/javascript";
 		d.head.appendChild(s);
 	}
--- ccplus.js
+++ ccplus.js
@@ -6,7 +6,7 @@
 	    $ccplus_fps;
 
 	var l = document.createElement("link");
-	l.href = "http://ccplus.localhost/ccplus.css";
+	l.href = "https://raw.github.com/ganglio/ccplus/master/ccplus.css";
 	l.type="text/css";
 	l.rel="stylesheet";
 	l.addEventListener("load",function(){
