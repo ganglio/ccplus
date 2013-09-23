00ded3121918817cedc9f13ff4359cb953d0aaea
Roberto Torella <roberto.torella@gmail.com>
2013-09-23 15:43:51 +0100

    Fixed stupid bug in JS

--- ccplus.js
+++ ccplus.js
@@ -1,9 +1,10 @@
 (function(){
-	var l = d.createElement("link");
+	var l = document.createElement("link");
 	l.href = "https://raw.github.com/ganglio/ccplus/master/ccplus.css";
 	l.type="text/css";
 	l.rel="stylesheet";
 	l.addEventListener("load",function(){
 		alert("Style Loaded");
 	});
+	document.head.appendChild(l);
 })();
