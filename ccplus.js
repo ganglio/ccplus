9011d5383dc29ed3b84aa9f28ddfd8e0486a5c4f
Roberto Torella <roberto.torella@gmail.com>
2013-09-23 15:41:39 +0100

    Small BML tweaks + a bit more complex JS

--- ccplus.js
+++ ccplus.js
@@ -1,3 +1,9 @@
 (function(){
-	alert("pesco");
+	var l = d.createElement("link");
+	l.href = "https://raw.github.com/ganglio/ccplus/master/ccplus.css";
+	l.type="text/css";
+	l.rel="stylesheet";
+	l.addEventListener("load",function(){
+		alert("Style Loaded");
+	});
 })();
