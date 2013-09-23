9de67ab0f0e149a7347d279d09fd57df88c8e21e
Roberto Torella <roberto.torella@gmail.com>
2013-09-23 17:11:18 +0100

    Added clickbot control

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,8 @@
 javascript: void((function(d){
-	if (!document.querySelector("#ccplus")) {
+	if (!document.querySelector("#ccplus-style")) {
 		var s = d.createElement("script");
-		s.id="ccplus";
-		s.src = "https://ccplus.localhost/ccplus.js";
+		s.id="ccplus-style";
+		s.src = "http://ccplus.localhost/ccplus.js";
 		s.type="text/javascript";
 		d.head.appendChild(s);
 	}
--- ccplus.js
+++ ccplus.js
@@ -1,10 +1,45 @@
 (function(){
+
+	"use strict";
+
+	var $ccplus_cheat,
+	    $ccplus_fps;
+
 	var l = document.createElement("link");
-	l.href = "https://ccplus.localhost/ccplus.css";
+	l.href = "http://ccplus.localhost/ccplus.css";
 	l.type="text/css";
 	l.rel="stylesheet";
 	l.addEventListener("load",function(){
-		alert("Style Loaded");
+		var ccplus = document.createElement("div");
+		ccplus.id = "ccplus";
+		ccplus.innerHTML = "<ul><li class='cheat'>ClickBot </li><li class='fps'></li></ul>";
+		document.body.appendChild(ccplus);
+		init();
 	});
 	document.head.appendChild(l);
+
+	function init() {
+		$ccplus_cheat = document.querySelector("#ccplus .cheat");
+		$ccplus_fps = document.querySelector("#ccplus .fps");
+		$ccplus_cheat.addEventListener("click",function(){
+			this.classList.toggle("on");
+		});
+	}
+
+	var filterStrength = 20;
+	var frameTime = 0, lastLoop = new Date, thisLoop;
+	var loop = function() {
+		if ( $ccplus_cheat && $ccplus_cheat.classList.contains("on"))
+			document.querySelector("#bigCookie").click();
+		var thisFrameTime = (thisLoop=new Date) - lastLoop;
+		frameTime+= (thisFrameTime - frameTime) / filterStrength;
+		lastLoop = thisLoop;
+
+		window.requestAnimationFrame(loop);
+	}
+	setInterval(function(){
+		if ( $ccplus_cheat && $ccplus_cheat.classList.contains("on"))
+			document.querySelector("#ccplus .fps").innerText= (1000/frameTime).toFixed(1);
+	},1000);
+	loop();
 })();
