024971a354201887f09e90875707f3e95d45a95f
Roberto Torella <robertot@square-enix.com>
2015-11-18 10:42:30 +0000

    Added Golden Hoard toggle button

--- ccplus.js
+++ ccplus.js
@@ -7,12 +7,29 @@
 
 	var l = document.createElement("link");
 	l.href = "http://ganglio.github.com/ccplus/ccplus.css";
+	// l.href = "http://localhost:8080/ccplus/ccplus.css?"+Math.random();
 	l.type="text/css";
 	l.rel="stylesheet";
 	l.addEventListener("load",function(){
 		var ccplus = document.createElement("div");
 		ccplus.id = "ccplus";
-		ccplus.innerHTML = "<ul><li class='cheat'>ClickBot <input type='checkbox' class='switch' data-on='1' data-off='0' name='cheat'> <span class='fps'></span></li><li class='mail'><span>Send Save</span></li></ul>";
+		ccplus.innerHTML =
+			"<ul>" +
+				"<li>" +
+					"<label for='cheat'>ClickBot</label>" +
+					"<input type='checkbox' id='cheat' class='switch' data-on='1' data-off='0' name='cheat'>" +
+				"</li>" +
+				"<li>" +
+					"<span class='fps'>0</span>fps" +
+				"</li>" +
+				"<li>" +
+					"<span class='hoard'>Hoard</span>" +
+				"</li>" +
+				"<li>" +
+					"<span class='mail'>Send Save</span>" +
+				"</li>" +
+			"</ul>"
+		;
 		document.body.appendChild(ccplus);
 		init();
 	});
@@ -26,6 +43,9 @@
 		document.querySelector("#ccplus .mail").addEventListener("click",function(){
 			window.open("mailto:?subject="+escape("Cookie Clicker Save")+"&body="+escape("This is your savefile on " + new Date() + ":\n\n" + Game.WriteSave(1)+"\n\nClick here http://orteil.dashnet.org/cookieclicker/ to play!"));
 		});
+		document.querySelector("#ccplus .hoard").addEventListener("click",function(){
+			Game.UpgradesById[83].toggle();
+		});
 	}
 
 	var filterStrength = 20;
@@ -36,16 +56,13 @@
 			for (var i=0;i<10;i++) {
 				if (Game.wrinklers[i].close==1) {
 					Game.wrinklers[i].hp = 0
-					console.log("wrinkler clicked");
 				}
 			}
 			if (Game.goldenCookie.life > 0) {
 				Game.goldenCookie.click();
-				console.log("goldenCookie clicked");
 			}
 			if (Game.seasonPopup.life > 0) {
 				Game.seasonPopup.click();
-				console.log("seasonPopup clicked");
 			}
 		}
 		var thisFrameTime = (thisLoop=new Date) - lastLoop;
